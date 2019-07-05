2d1ce3927da9f7310892299cb2a3cccf6aa8ba34
santanu chowdhury <40321346+santanuhtml@users.noreply.github.com>
2019-07-05 14:29:51 +0530

    Update Update.js

--- Update.js
+++ Update.js
@@ -71,3 +71,73 @@ $('#contact-form-submit3').on('click',function(){
     })
   }
 });
+
+
+/*
+-----------------------------------------------
+REDIRECT TO ANOTHER PAGE
+-----------------------------------------------
+*/
+
+$('#contact-form-submit').on('click',function(){
+  if($("#contact-form").valid())
+  {
+    var name = $("#conName").val();
+    var email = $("#conEmail").val();
+    var phone = $("#conPhone").val();
+    var subject = "enquiry mail";
+    var message = $("#conMessage").val();
+    var firstName = name;
+    
+    if (firstName.indexOf(' ') >= 0) {
+        firstName = name.split(' ').slice(0, -1).join(' ');
+    }
+    $.ajax({
+        url: "./mail/contact_me.php",
+        type: "POST",
+        data: {
+            name: name,
+            email: email,
+            subject: subject,
+            phone: phone,
+            message: message
+        },
+        success: function(response){
+            console.log(response);
+          if(response == 'success')
+          {
+            $('#submitMessage').html("<div class='alert alert-success'>");
+            $('#submitMessage > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                .append("</button>");
+            $('#submitMessage > .alert-success')
+                .append("<strong>Your message has been sent. </strong>");
+            $('#submitMessage > .alert-success')
+                .append('</div>');
+            $('#submitMessage').delay(3000).fadeOut();
+            //clear all fields
+            $('#contact-form').trigger("reset");
+            
+             /* =================== redirect to another page =================== */
+            var url = "http://www.spacelifestyle.com/aqua-beaumont/thankyou.php";
+            $(location).attr('href',url);
+             /* =================== /redirect to another page =================== */
+
+          }
+          else{
+            $('#submitMessage').html("<div class='alert alert-danger'>");
+            $('#submitMessage > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                .append("</button>");
+            $('#submitMessage > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+            $('#submitMessage > .alert-danger').append('</div>');
+            $('#submitMessage').delay(3000).fadeOut();
+            //clear all fields
+            $('#contact-form').trigger("reset");
+            
+          }
+        }
+    })
+  }
+});
+
+
+
